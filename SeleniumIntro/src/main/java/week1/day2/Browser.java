5801f940a5d215fea366e4174b6eba233f47302b
fleenchris <fleenchris@localhost>
2025-02-18 06:25:02 -0800

    Second day assignment

---
+++ SeleniumIntro/src/main/java/week1/day2/Browser.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class Browser {
+	
+	public void launchBrowser()
+	{
+		System.out.println("Browser launched sucessfully");		
+	}
+
+	public void loadURL()
+	{
+		System.out.println("Application URL loaded Successfully");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Browser obj = new Browser();
+		obj.launchBrowser();
+		obj.loadURL();
+		
+		
+
+	}
+
+}
---
+++ SeleniumIntro/src/main/java/week1/day2/Firefox.java
@@ -0,0 +1,32 @@
+package week1.day2;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 Float browserVersion = 100.2f;
+		 
+		 String browserName = "firefox";
+
+		 Boolean isVsible = true;
+		 
+		 int releaseYear = 1998;
+		 
+		 char browserLogo = 'f';
+		 
+		 System.out.println("Browser Version:"+browserVersion);
+		 
+		 System.out.println("Browser Name:"+browserName);
+		 
+		 System.out.println("Is Browser Vsible :"+isVsible);
+		 
+		 System.out.println("Browser Release Year :"+releaseYear);
+		 
+		 System.out.println("Browser Logo :"+browserLogo);
+		 
+		 
+
+	}
+
+}
