6f50c60f13bf09aa3eec01c889a4a650d65c8a2e
fleenchris <fleenchris@192.168.1.8>
2025-03-12 09:57:38 -0700

    Week 4 day 3

--- SeleniumIntro/src/main/java/week4/day3/SalesForceTestcaseTwo.java
+++ SeleniumIntro/src/main/java/week4/day3/SalesForceTestcaseTwo.java
@@ -12,16 +12,28 @@ public class SalesForceTestcaseTwo extends SalesForceProjSpecMethods {
 	public void nameAsbyMyName() throws InterruptedException {
 		
 	 Thread.sleep(1000);
+
+     WebElement sfName = driver.findElement(By.xpath("(//div[@class='actionBody']//dl[@class='slds-form']//records-record-layout-item//div[@part='input-text']//div)[1]"));
+     
+     sfName.sendKeys("Salesforce Automation by Fleena");
 	
-	WebElement sfCompName = driver.findElement(By.xpath("(//div[@class='actionBody']//dl[@class='slds-form']//records-record-layout-item//div[@part='input-text']//div)[2]"));
+     WebElement sfCompName = driver.findElement(By.xpath("(//div[@class='actionBody']//dl[@class='slds-form']//records-record-layout-item//div[@part='input-text']//div)[2]"));
     
-	//sfCompName.sendKeys("TestLeaf");
-	JavascriptExecutor js = (JavascriptExecutor) driver;
-	js.executeScript("arguments[0].value='TestLeaf';", sfCompName);
+     //sfCompName.sendKeys("TestLeaf");
+     JavascriptExecutor js = (JavascriptExecutor) driver;
+     js.executeScript("arguments[0].value='TestLeaf';", sfCompName);
 	
 	
      WebElement sfDiscrip = driver.findElement(By.xpath("(//div[@class='slds-form-element__control slds-grow textarea-container'])[2]/textarea"));
      sfDiscrip.sendKeys("Salesforces");
+     
+     WebElement dpNone = driver.findElement(By.xpath("//span[@class='slds-truncate'][@part='input-button-value']"));
+     js.executeScript("arguments[0].click();", dpNone);
+     
+     Thread.sleep(1000);
+     
+     driver.findElement(By.xpath("//span[@class='slds-media__body']/span[text()='Active']")).click();
     
+     driver.findElement(By.xpath("//ul[@class='slds-button-group-row']//button[@name='SaveEdit']")).click();
 
 }}
